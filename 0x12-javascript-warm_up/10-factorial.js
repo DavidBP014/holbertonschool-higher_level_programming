229434379a2f49a2cc0d06ca414973900d23143b
Juan David Ballesteros Perez <91141739+DavidBP014@users.noreply.github.com>
2022-08-15 22:01:33 -0500

    correct

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-function factorialize(num) {
-    let MyRes = 1;
-    for (let i = 1; i <= num; i++) {
-        MyRes *= i;
-    }
-    return (MyRes);
+const x = process.argv[2];
+function factorial (x) {
+  if (isNaN(x) || x === 1) {
+    return (1);
+  } else {
+    return (x * factorial(x - 1));
+  }
 }
-console.log(factorialize(parseInt(process.argv[2])));
+console.log(factorial(parseInt(x)));
