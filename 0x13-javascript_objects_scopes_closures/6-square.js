4502f2e9a78f2b0a5809b6c66b83dd1b8070da43
Juan David Ballesteros Perez <91141739+DavidBP014@users.noreply.github.com>
2022-08-16 23:12:12 -0500

    error 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,18 +2,18 @@
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-    constructor(size) {
-        super(size, size);
-    }
+  constructor (size) {
+    super(size, size);
+  }
 
-    charPrint(c) {
-        if (c) {
-            for (let i = 0; i < this.height; i++) {
-                console.log(c.repeat(this.width));
-            }
-        } else {
-            this.print();
-        }
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      this.print();
     }
+  }
 }
 module.exports = Square;
