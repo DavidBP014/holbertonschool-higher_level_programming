3663503681af9bdf751f8c28ea74067c70bf05a9
Juan David Ballesteros Perez <91141739+DavidBP014@users.noreply.github.com>
2022-08-17 22:16:10 -0500

    klo

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
+const file = process.argv[2];
+const data = process.argv[3];
+fs.writeFile(file, data, (error) => {
+  if (error) {
+    console.log(error);
+  }
 });
